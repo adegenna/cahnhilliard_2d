a644963ce3d7a14366c270cedb91e5ea2772f4c4
Anthony DeGennaro <adegennaro@bnl.gov>
2019-02-17 14:26:58 -0500

    Updates

--- docs/source/intro.rst
+++ docs/source/intro.rst
@@ -1,6 +1,4 @@
 Introduction
 =====================
 
-The following project is a Python implementation of the YOLOv3 object detection algorithm. This specific software began as a project intended for use with the Xview dataset specifically by Glenn Jocher at Ultralytics (https://github.com/ultralytics/xview-yolov3.git). It has since been modified extensively for the purposes of generality, maintainability, and usability by Anthony DeGennaro at Brookhaven National Laboratory (https://www.bnl.gov/compsci/people/staff.php?q=168 / adegennaro@bnl.gov).
-
-You may access the main project repository at https://github.com/adegenna/xview-yolov3.
+The following project is a Python implementation of a solver for the 2D Cahn Hilliard equation. You may access the main project repository at https://github.com/adegenna/cahnhilliard-2d.
--- visualization/plotContourState.py
+++ visualization/plotContourState.py
@@ -1,22 +1,64 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.fftpack import dct
+
+def dct2d(x,inverse=False):
+    t    = 2 if not inverse else 3
+    temp = dct(x,type=t,norm='ortho').transpose()
+    return dct(temp,type=t,norm='ortho').transpose()
 
 statefile   = '/home/adegennaro/Projects/AEOLUS/cahnhilliard_2d/output/state.csv'
 M           = 128
 N           = 128
+C           = np.genfromtxt(statefile)
+samps       = C.shape[0]
+
+# Precompute fourier cosine spectrum
+dct_c  = np.zeros([samps,M,N])
+for i in range(samps):
+    Ci       = np.reshape(C[i],[M,N])
+    dct_c[i] = dct2d(Ci)
 
 # State plot, colored by time
-C     = np.genfromtxt(statefile)
-samps = C.shape[0]
-plt.figure(1)
+fig   = plt.figure(1,figsize=(20,10))
+ax1   = fig.add_subplot(221)
+ax2   = fig.add_subplot(222)
+ax3   = fig.add_subplot(223)
 plt.ion()
 for i in range(samps):
     Ci = np.reshape(C[i],[M,N])
-    plt.clf()
-    plt.contourf(Ci,30,vmin=-1,vmax=1)
-    plt.title(i)
+    if (i != 0):
+        Cim1 = np.reshape(C[i-1],[M,N])
+        dcdt = np.linalg.norm(Ci-Cim1)
+    else:
+        dcdt = 0
+    ax1.cla()
+    ax1.contourf(Ci,30,vmin=-1,vmax=1)
+    ax1.set_title(i)
+    ax2.scatter(i+1,dcdt,c='b')
+    ax2.set_yscale('log')
+    ax2.set_ylim([1,100])
+    ax2.set_title('dC/dt')
+    ax3.cla()
+    ax3.contourf(np.log10(np.abs(dct_c[i])),30,vmin=-2,vmax=2)
+    ax3.set_title('log10(DCT(C))')
     plt.pause(0.01)
 
+# Static plot of 3 different times
 plt.ioff()
-plt.show()
+fig   = plt.figure(2,figsize=(20,10))
+ax1   = fig.add_subplot(231)
+ax2   = fig.add_subplot(234)
+ax3   = fig.add_subplot(232)
+ax4   = fig.add_subplot(235)
+ax5   = fig.add_subplot(233)
+ax6   = fig.add_subplot(236)
+ax    = [ax1,ax2,ax3,ax4,ax5,ax6]
+T     = [5,25,100]
+tr    = 32
+for i in range(len(T)):
+    ax[2*i].contourf( np.reshape(C[T[i]],[M,N]) , 30, vmin=-1, vmax=1 )
+    ax[2*i+1].contourf( np.log10(np.abs(dct_c[T[i]]))[0:tr,0:tr],30,vmin=-1,vmax=1 )
+    ax[2*i].set_title(T[i])
 
+plt.show()
