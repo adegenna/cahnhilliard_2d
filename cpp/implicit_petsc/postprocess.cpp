7ff519358eb26eb529d36903da798e8de02fe610
Anthony DeGennaro <adegennaro@bnl.gov>
2019-09-11 09:36:25 -0400

    Update postprocess to accept single binary file cmdline input

--- cpp/implicit_petsc/postprocess.cpp
+++ cpp/implicit_petsc/postprocess.cpp
@@ -14,13 +14,17 @@ static char help[] = "JFNK implicit solver for 2D CH with PETSc \n";
 #include "utils_ch_implicit.h"
 
 int main(int argc,char **argv) {
+
+  // Interface: ./postprocess <petsc_config_file.dat> <parallel_binary_file.bin>
   
   Vec            U;                    /* solution, residual vectors */
   PetscErrorCode ierr;
   DM             da;
   SNES           snes;
 
-  ierr = PetscInitialize(&argc,&argv,argv[1],help);if (ierr) return ierr;
+  ierr = PetscInitialize( NULL , NULL , argv[1] , help );if (ierr) return ierr;
+  
+  const std::string solnfile = argv[2];
 
   AppCtx         user = parse_petsc_options();
 
@@ -40,32 +44,27 @@ int main(int argc,char **argv) {
   user.da = da;
 
   /*  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-     Extract global vectors from DMDA;
+     Convert parallel binary --> serial ascii
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-  DMCreateGlobalVector(da,&U);  
+  DMCreateGlobalVector(da,&U);
 
   MPI_Comm       comm = PETSC_COMM_WORLD;
-  PetscInt num_solns  = (int) user.t_final/user.dt_output + 1;
-  for (int i=0; i<num_solns; i++) {
 
-    // Load original MPI solnfile
-    const std::string solnfile = "c_" + std::to_string( user.dt_output * i ).substr(0,6) + ".bin";
-    PetscViewer    viewer_input;
-    PetscViewerBinaryOpen( comm , solnfile.c_str() , FILE_MODE_READ , &viewer_input );
-    VecLoad( U , viewer_input );
-    PetscViewerDestroy( &viewer_input );
+  // Load original MPI solnfile
+  PetscViewer    viewer_input;
+  PetscViewerBinaryOpen( comm , solnfile.c_str() , FILE_MODE_READ , &viewer_input );
+  VecLoad( U , viewer_input );
+  PetscViewerDestroy( &viewer_input );
 
-    // Rewrite in serial to ASCII
-    PetscViewer viewer_output;
-    const std::string fileoutname = "c_" + std::to_string( user.dt_output * i ).substr(0,6) + ".ascii";
-    PetscViewerCreate( comm , &viewer_output );
-    PetscViewerSetType( viewer_output , PETSCVIEWERASCII );
-    PetscViewerFileSetMode( viewer_output , FILE_MODE_WRITE );
-    PetscViewerASCIIOpen( comm , fileoutname.c_str() , &viewer_output );
-    VecView( U , viewer_output );
-    PetscViewerDestroy( &viewer_output );
-
-  }  
+  // Rewrite in serial to ASCII
+  PetscViewer viewer_output;
+  const std::string fileoutname = solnfile.substr( 0 , solnfile.size()-4 ) + ".ascii";
+  PetscViewerCreate( comm , &viewer_output );
+  PetscViewerSetType( viewer_output , PETSCVIEWERASCII );
+  PetscViewerFileSetMode( viewer_output , FILE_MODE_WRITE );
+  PetscViewerASCIIOpen( comm , fileoutname.c_str() , &viewer_output );
+  VecView( U , viewer_output );
+  PetscViewerDestroy( &viewer_output );
 
   VecDestroy(&U);
   DMDestroy(&da);
