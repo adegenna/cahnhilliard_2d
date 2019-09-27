fd5b43be119b6c16344e4f7ddbff76ac455182e3
Anthony DeGennaro <adegennaro@bnl.gov>
2019-09-27 17:27:51 -0400

    Start development for coupled thermal ch

--- cpp/implicit_petsc/jfnk_2d.cpp
+++ cpp/implicit_petsc/jfnk_2d.cpp
@@ -20,20 +20,23 @@ static char help[] = "JFNK implicit solver for 2D CH with PETSc \n";
 int main(int argc,char **argv) {
   
   TS             ts;                   /* nonlinear solver */
-  Vec            u,r;                  /* solution, residual vectors */
+  Vec            u,c,r;                /* solution, residual vectors */
   Mat            J,Jmf = NULL;         /* Jacobian matrices */
   PetscErrorCode ierr;
-  DM             da;
+  DM             da_c;
   PetscReal      dt;
   SNES           snes;
+  KSP            ksp;
 
-  ierr = PetscInitialize(&argc,&argv,argv[1],help);if (ierr) return ierr;
+  PetscInitialize(&argc,&argv,argv[1],help);
 
   AppCtx         user = parse_petsc_options();
 
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
      Create distributed array (DMDA) to manage parallel grid and vectors
   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */  
+
+  // DM for concentration c
   DMDACreate2d(PETSC_COMM_WORLD, 
                DM_BOUNDARY_GHOSTED, DM_BOUNDARY_GHOSTED,    // type of boundary nodes
                DMDA_STENCIL_BOX,                // type of stencil
@@ -41,60 +44,121 @@ int main(int argc,char **argv) {
                PETSC_DECIDE,PETSC_DECIDE,       // #procs in each dimn
                1,                               // DOF per node
                2,                               // Stencil width
-               NULL,NULL,&da);
-  DMSetFromOptions(da);
-  DMSetUp(da);
-  user.da = da;
+               NULL,NULL,&da_c);
+  DMSetFromOptions(da_c);
+  DMSetOptionsPrefix(da_c,"c_");
+  DMSetUp(da_c);
 
+  // Get process ownership ranges so that you can link different physics with the same indices
+  const PetscInt *lxc , *lyc;
+  PetscInt *lxT , *lyT;
+  PetscInt sizes_x , sizes_y;
+  PetscInt nx , ny;
+  DMDAGetOwnershipRanges( da_c , &lxc , &lyc , 0 );
+  DMDAGetInfo( da_c , NULL, &nx,&ny,NULL, &sizes_x,&sizes_y,NULL, NULL,NULL,NULL,NULL,NULL,NULL );
+  PetscMalloc1( sizes_x , &lxT );
+  PetscMalloc1( sizes_y , &lyT );
+  PetscMemcpy( lxT , lxc , sizes*sizeof(*lxT) );
+  PetscMemcpy( lyT , lyc , sizes*sizeof(*lyT) );
+  lxT[0]--;
+  lyT[0]--;
+  
+  // DM for temperature T
+  DMDACreate2d(PETSC_COMM_WORLD, 
+               DM_BOUNDARY_GHOSTED, DM_BOUNDARY_GHOSTED,    // type of boundary nodes
+               DMDA_STENCIL_STAR,                // type of stencil
+               nx,ny,                           // global dimns of array
+               PETSC_DECIDE,PETSC_DECIDE,       // #procs in each dimn
+               1,                               // DOF per node
+               1,                               // Stencil width
+               NULL,NULL,&da_T);
+  DMSetFromOptions(da_T);
+  DMSetOptionsPrefix(da_T,"T_");
+  DMSetUp(da_T);
+  
+  PetscFree(lxT);
+  PetscFree(lyT);
+
+  // Pack the DMs together into a single composite manager
+  DMCompositeCreate( PETSC_COMM_WORLD , &pack );
+  DMSetOptionsPrefix( pack , "pack_" );
+  DMCompositeAddDM( pack , da_c );
+  DMCompositeAddDM( pack , da_T );
+  DMDASetFieldName( da_c , 0 , "c" );
+  DMDASetFieldName( da_T , 0 , "T" );
+  DMSetFromOptions( pack );
+  
   // Rescale value of L_omega to match the user-specified domain size
   PetscScalar L_domain = sqrtf( user.Lx * user.Ly );
   user.L_omega        *= L_domain;
 
+  user.da_c = da_c;
+  user.pack = pack;
+  
   /*  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
      Extract global vectors from DMDA;
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-  DMCreateGlobalVector(da,&u);  
-  VecDuplicate(u,&r);
-  VecDuplicate(u,&user.eps_2);
-  VecDuplicate(u,&user.sigma);
-  VecDuplicate(u,&user.temperature);
-  VecDuplicate(u,&user.X);
+  DMCreateGlobalVector( da_c , &c );
+  DMCreateGlobalVector( pack , &u );
+  VecDuplicate( c , &r );
+  VecDuplicate( c , &r );
+  VecDuplicate(c,&user.eps_2);
+  VecDuplicate(c,&user.sigma);
+  VecDuplicate(c,&user.temperature);
+  VecDuplicate(c,&user.X);
   
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
      Create timestepping solver context
      - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
   TSCreate(PETSC_COMM_WORLD,&ts);
   TSSetProblemType(ts,TS_NONLINEAR);
-  TSSetDM(ts,da);
-  TSSetIFunction(ts,r,FormIFunction,&user);
   TSSetMaxTime(ts,user.t_final);
   TSSetExactFinalTime(ts,TS_EXACTFINALTIME_STEPOVER);
-
-  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-     Set initial conditions
-   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-  FormInitialSolution( u , user.temperature , &user );
-  compute_eps2_and_sigma_from_temperature( &user );
-  TSSetSolution(ts,u);
   dt   = 0.005;
   TSSetTimeStep(ts,dt);
 
-  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-   Set Jacobian evaluation routine
-  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-  ierr  = DMSetMatType(da,MATAIJ);
-  ierr  = DMCreateMatrix(da,&J);
-  TSGetSNES(ts,&snes);
-  MatCreateSNESMF(snes,&Jmf);
-  SNESSetJacobian(snes,Jmf,J,SNESComputeJacobianDefaultColor,0);
+  // Thermal CH parameters
+  compute_eps2_and_sigma_from_temperature( &user );
 
   /*  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-   Sets various TS parameters from user options
+   Set options based on type of physics
+   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  if (user.physics = 0) {
+    // CH only
+
+    VecDuplicate( c , &r );
+
+    // TS
+    TSSetDM(ts,da_c);
+    TSSetIFunction(ts,r,FormIFunction,&user);
+    FormInitialSolution( u , user.temperature , &user );
+    TSSetSolution(ts,u);
+
+    // SNES
+    DMSetMatType(da_c,MATAIJ);
+    DMCreateMatrix(da_c,&J);
+    TSGetSNES(ts,&snes);
+    MatCreateSNESMF(snes,&Jmf);
+    SNESSetJacobian(snes,Jmf,J,SNESComputeJacobianDefaultColor,0);
+    
+  }
+  else if (user.physics == 1) {
+    // Coupled CH + thermal
+    
+    VecDuplicate( u , &r );
+
+  }
+
+  /*  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+   Set things from user-provided PETSc options
    - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
   TSSetFromOptions(ts);
-  PetscOptionsView( NULL , PETSC_VIEWER_STDOUT_WORLD );
+  SNESSetFromOptions(snes);
+  SNESGetKSP(snes,&ksp);
+  KSPSetFromOptions(ksp);
+  PetscOptionsView( NULL , PETSC_VIEWER_STDOUT_WORLD );  
 
-  /* Set directions and terminate flags for the two events */
+  // Setup event handling
   PetscInt       direction[2];
   PetscBool      terminate[2];
   direction[0] = 1; direction[1] = 1;
@@ -115,12 +179,6 @@ int main(int argc,char **argv) {
   log_solution( u , final_soln );
   
   PetscPrintf( PETSC_COMM_WORLD , "SIMULATION DONE\n\n" );
-
-  // Output to filesystem that you are done
-  // const std::string outname  = "complete_sim.out";
-  // std::ofstream fout(outname);
-  // fout << "whole simulation complete\n";
-  // fout.close();
   
   /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
      Free work space.
@@ -128,13 +186,14 @@ int main(int argc,char **argv) {
   MatDestroy(&J);
   MatDestroy(&Jmf);
   VecDestroy(&u);
+  VecDestroy(&c);
   VecDestroy(&r);
   VecDestroy(&user.eps_2);
   VecDestroy(&user.sigma);
   VecDestroy(&user.temperature);
   VecDestroy(&user.X);
   TSDestroy(&ts);
-  DMDestroy(&da);
+  DMDestroy(&da_c);
 
   PetscFinalize();
   return ierr;
