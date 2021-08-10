4f54754d776b9729dd34d1c3da6e5d4f1425d20f
Anthony DeGennaro <amdegennaro@gmail.com>
2021-08-10 19:47:46 -0400

    branch off master for minimal project (no petsc)

--- cpp/implicit_petsc/initial_conditions.h
+++
@@ -1,9 +0,0 @@
-#ifndef __INITIAL_CONDITIONS_H__
-#define __INITIAL_CONDITIONS_H__
-
-#include "utils_ch_implicit.h"
-
-PetscErrorCode FormInitialSolution( Vec U , Vec eps_2 , void *ptr );
-
-
-#endif
--- visualization/video.py
+++ visualization/video.py
@@ -1,28 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import animation
+import matplotlib.animation as animation
 
-def read_mpi_soln_file( statefile , timestamp , n ):
+def read_swig_soln_file( statefile , tstep , i ):
     
-    c = np.zeros( n )
-    count = 0
-    with open( statefile + timestamp ) as f:
-        for line in f:
-            try:
-                c[count] = line
-                count += 1
-            except:
-                pass
-    
-    return c
+    filenametotal = statefile + str(i) + '.out'
+    print( filenametotal )
+    return np.genfromtxt( filenametotal )
 
 t0 = 0.00
 tf = 1.00
-#statefile   = '/home/adegennaro/Projects/AEOLUS/cahnhilliard_2d/cpp/build/C_'
-#statefile   = '/home/adegennaro/Projects/AEOLUS/cahnhilliard_2d/cpp/swig/C_'
-statefile   = '/home/adegennaro/Projects/AEOLUS/cahnhilliard_2d/data/mixingmorphologies/dots/c_'
-#statefile   = '/home/adegennaro/Projects/AEOLUS/cahnhilliard_2d/cpp/implicit_petsc/examples/c_'
-
+statefile   = '/home/adegennaro/Projects/AEOLUS/cahnhilliard_2d/cpp/swig/C_'
 nx = 64
 ny = 64
 
@@ -31,26 +19,19 @@ y     = np.arange(ny)
 xx,yy = np.meshgrid(x,y)
 xx = xx.T; yy = yy.T
 
-tstep = 0.02
+tstep = 0.01
 fig   = plt.figure(10,figsize=(8,8))
 ax    = fig.gca()
 
 # animation function.  This is called sequentially
-def animate(i):
-    print(i*tstep + t0)
-    #w = np.genfromtxt(statefile + str(int(i*tstep)) + '.out' )
-    timestamp = '{:0.4f}.ascii'.format( (i*tstep + t0) )
-    w = read_mpi_soln_file( statefile , timestamp , nx*ny )
-    #w = np.genfromtxt(statefile + timestamp , skip_header=3 )
-    w = w.reshape([nx,ny],order='C');
-    ax.cla()
-    contour = ax.contourf(xx,yy,w,30,vmin=-0.4,vmax=0.4)
-    ax.set_aspect('equal')
-    #contour = ax.contourf(xx,yy,w,30,vmin=0.,vmax=1)
-    return contour
 
-anim = animation.FuncAnimation(fig, animate,
-                               frames=int((tf-t0)/tstep + 1), interval=20)
-anim.save('ch2d.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
-#plt.show()
+frames = []
 
+for i in range(int((tf-t0)/tstep + 1)):
+    w = read_swig_soln_file( statefile , tstep , i )
+    w = w.reshape([nx,ny],order='C')
+
+    frames.append( ( ax.contourf( xx,yy,w,30,vmin=-0.4,vmax=0.4, cmap='afmhot') ).collections )
+
+ani = animation.ArtistAnimation(fig, frames, interval=20 , blit=True )
+ani.save('ch2d.mp4' )
