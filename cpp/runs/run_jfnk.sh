f5840bb82dc58f5e43a9388d1c9d17206eb6a423
Anthony DeGennaro <amdegennaro@gmail.com>
2021-08-27 14:34:43 -0400

    delete petsc examples

--- cpp/runs/run_jfnk.sh
+++
@@ -1,7 +0,0 @@
-export petsc_inputfile="/home/adegennaro/cahnhilliard_2d/cpp/implicit_petsc/examples/petscrc.dat"
-export mpiexec_petsc="/home/adegennaro/cahnhilliard_2d/cpp/build/external/petsc/arch-linux2-c-opt/bin/mpiexec"
-
-echo "PETSc inputfile =" $petsc_inputfile
-
-# Run solver
-$mpiexec_petsc ./jfnk_2d $petsc_inputfile
